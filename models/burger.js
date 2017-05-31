b56940a134e6e07327ae900d14f4aeabdaa623f8
Ryan Rodwell <ryanrodwell@gmail.com>
2017-05-31 16:32:27 -0400

    fixing sequelize datatype

--- models/burger.js
+++ models/burger.js
@@ -2,12 +2,14 @@
  * Created by ryanrodwell on 5/17/17.
  */
 module.exports = function(sequelize, DataTypes){
-    var Burger = sequelize.define("Burger",{
+    var Burger = sequelize.define("Burger", {
         burger_name: DataTypes.STRING,
         devoured: {
             type: DataTypes.BOOLEAN,
             defaultValue: false
-    }
+        }
+    }, {
+        timestamps: false
     });
     return Burger;
 };
